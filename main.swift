b03540a7afc20b95e954a79b770dc983680355d5
a493110804 <493110804@qq.com>
2016-06-20 10:26:37 +0800

    从一加到一百
    通过视频学习，学会了基本的swift语句，运用var对i和m赋值，再运用while循环实现从一到一百的累加。

---
+++ main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  loop
+//
+//  Created by 魏新宇 on 16/6/20.
+//  Copyright © 2016年 魏新宇. All rights reserved.
+//
+
+import Foundation
+
+var i=0
+var m=0
+while i<100{
+    i++
+    m=m+i
+}
+print(m)
+
